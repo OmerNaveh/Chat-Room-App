f3286a0bd4be6976a3edefe629cfca061780de55
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-12-02 09:46:09 +0200

    created Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,19 @@
 export default function Footer(props){
     return(
-        <div>
-            
+        <div className="footer">
+            <div className="creator">
+            <div className="creatorDet">
+                <p>Omer Naveh</p>
+                <div className="creatorLinks">
+                <a href="https://github.com/OmerNaveh">
+                    <i className="fab fa-github-square gitLogo"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/omer-naveh-900941225/">
+                    <i className="fab fa-linkedin gitLogo"></i>
+                </a>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
