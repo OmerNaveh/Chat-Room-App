7fe6279dd78b7815f6e64e6c890263e1af3f60d2
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-12-04 10:34:19 +0200

    built messages api

--- back/server.js
+++ back/server.js
@@ -1,7 +1,13 @@
 const express= require('express');
+const cors = require('cors')
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000
+const userRouter = require('./routers/user')
+const messagesRouter = require('./routers/messages')
 
+app.use(cors());
 app.use(express.json())
 app.listen(port, ()=>{console.log(`running on ${port}`)})
+app.use('/users', userRouter)
+app.use('/meesages', messagesRouter)
